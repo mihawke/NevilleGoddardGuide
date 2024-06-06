8b2ed704d99b920b761217b2b9cbbb6749c20e5c
mihawke <pratikgawari0000@gmail.com>
2024-06-06 14:24:20 +0530

    Added link

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Introduction = () => {
     return (
         <div className='flex flex-col items-center gap-y-2'>
-            <img src='src/assets/neville.jpeg' width={200} />
+            <img src='https://i0.wp.com/www.harvbishop.com/wp-content/uploads/2016/06/Neville-cosmic-1.jpg' width={200} />
             <ul className='flex flex-col text-left text-md'>
                 <li className='list-disc'>Neville Goddard (1905-1972) was a prominent teacher and author known for his works on mysticism, spirituality, and the power of imagination.</li>  <br />
                 <li className='list-disc'>Neville Goddard's teachings have influenced many modern self-help and New Thought movements, and his works continue to inspire those interested in personal development, spirituality, and the art of manifestation.</li>
